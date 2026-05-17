6dccb8c65f7101713d1986345441468daf5f72d4
Harsh Renose <renoseharsh@gmail.com>
2026-05-17 17:14:14 +0530

    setup cli browser for http, https and custom port number

---
+++ browser.py
@@ -0,0 +1,79 @@
+import socket
+import ssl
+
+
+class URL:
+    def __init__(self, url: str) -> None:
+        self.schema, url = url.split("://", 1)
+        assert self.schema in ["http", "https"]
+        if self.schema == "http":
+            self.port = 80
+        elif self.schema == "https":
+            self.port = 443
+
+        if "/" not in url:
+            url = url + "/"
+        self.host, url = url.split("/", 1)
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+        self.path = "/" + url
+
+    def request(self) -> str:
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP,
+        )
+        s.connect((self.host, self.port))
+
+        if self.schema == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        request = f"GET {self.path} HTTP/1.0\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        _, _, _ = response.readline().split(" ", 2)
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        content = response.read()
+        s.close()
+        return content
+
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+
+def show(body: str):
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            print(c, end="")
+
+
+if __name__ == "__main__":
+    import sys
+
+    load(URL(sys.argv[1]))
+
+# http://example.org/
